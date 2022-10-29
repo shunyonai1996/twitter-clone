5980022ad5d2e402ad57fdc6315a498ea5531cf5
shunyonai1996 <y_hhns_1028@icloud.com>
2022-10-29 09:13:18 +0900

    ユーザー登録機能実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
